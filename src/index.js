309caff3d2e547186e81c397e43a37265c741f7a
JovicAbengona <jvlabengona@gmail.com>
2022-05-25 17:23:25 +0800

    Add state for error handling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.state = { latitude: null, error: null };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { this.setState({ latitude: position.coords.latitude }); },
+            (err)      => { this.setState({ error: "CAN'T GET CURRENT LOCATION" }); }
+        );
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render() {
+        return <div><div>Latitude: { this.state.latitude }</div> <div>ERROR: { this.state.error }</div></div>
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
